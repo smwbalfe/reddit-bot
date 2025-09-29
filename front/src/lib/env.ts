516dedf4b59e4d939cfbb09a0c75017bf4d95192
Simon Balfe <smwbalfe@gmail.com>
2025-09-29 19:50:34 +0100

    fix

--- front/src/lib/env.ts
+++ front/src/lib/env.ts
@@ -21,8 +21,8 @@ const env = {
     
     DATABASE_URL: process.env.DATABASE_URL!,
     
-    UPSTASH_REDIS_REST_URL: process.env.UPSTASH_REDIS_REST_URL!,
-    UPSTASH_REDIS_REST_TOKEN: process.env.UPSTASH_REDIS_REST_TOKEN!,
+    UPSTASH_REDIS_REST_URL: process.env.UPSTASH_REDIS_REST_URL || '',
+    UPSTASH_REDIS_REST_TOKEN: process.env.UPSTASH_REDIS_REST_TOKEN || '',
     
     NEXT_PUBLIC_POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY!,
     NEXT_PUBLIC_POSTHOG_HOST: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
